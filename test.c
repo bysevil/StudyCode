fc9d1964e6b3406e7d9a8651dd2802de297e90a7
bysevil <1024530748@qq.com>
2023-07-31 19:58:01 +0800

    0731

--- test.c
+++ test.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
 
 int main() {
-    int flag[2][2] = {1,2,3,4};
-    for(int i =0;i< 4;i++)
-        printf("%d ",flag[i]);
-    return 0;
+    char a = '*';
+    printf("%d",a);
 }
