39f21ff55983745e8904bb82ba1acdfded31d4dc
bysevil <1024530748@qq.com>
2023-08-01 21:52:50 +0800

    0728

---
+++ 0801/bit721.c
@@ -0,0 +1,14 @@
+/*输出指定图案*/
+int main(){
+    int flag = 1;
+    for(int i =0;i>=0;){
+        for(int j =0;j<13;j++){
+                if(j>6+i || j < 6-i) printf(" ");
+                else printf("*");
+        }
+        if(i == 6) flag = 0;
+        flag?i++:i--;
+        printf("\n");
+    }
+    return 0;
+}
