67ac3e87b2a03283a5c6e7c5f6669a3b4417de9a
bysevil <1024530748@qq.com>
2023-08-04 19:20:25 +0800

    0804

--- test.c
+++ test.c
@@ -1,6 +1,21 @@
-#include<stdio.h>
+class
 
+
+#include<stdio.h>
 int main() {
-    char a = '*';
-    printf("%d",a);
+    int num = 0;
+    int sum = 0;
+    char arr[] = {17,13,15,19,14,16,11} ;
+    int arr2[] = {0};
+    for(int i =0;i<7;i++){
+        num
+        if(arr[i] > num) {
+            arr2[i] = arr[i] - num;
+            num += arr2[i];
+            sum += arr2[i];
+        }else{
+            arr2[i] = 0;
+        }
+    }
 }
+//arr[1] + arr[2] + arr[3] + arr[4] +arr[5] >= arr2[5] 
